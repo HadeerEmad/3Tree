9c7fb877a54f28ce282d2f60222e0faab9f0d25d
Hadeer Emad <23742626+HadeerEmad@users.noreply.github.com>
2022-06-03 22:09:45 +0300

    Add files via upload
    Mail edit

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $subject = "New Message form 3Tree website";
 $message = $_POST['message'];
 
-$formContent = "From: $name \n Email: $email  \n Message: $message";
+$formContent = "From: $name \nEmail: $email  \nMessage: $message";
 
 $recipient = "hadeer.cis@gmail.com"; // write email here
 $mailHeader = "From: $email \r\n";
